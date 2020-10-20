4e1084142eb498bce476488b0fad9832f71e6998
Fuyang Liu <fuyangl@spotify.com>
2020-10-20 21:31:59 +0200

    chap spawning - WIP

---
+++ my-redis/examples/hello-redis.rs
@@ -0,0 +1,18 @@
+use mini_redis::{client, Result};
+// use tokio_compat_02::FutureExt; // To try with tokio 0.3, needs the compat() method from this trait to let tokio 0.2 code working. https://tokio.rs/blog/2020-10-tokio-0-3
+
+#[tokio::main]
+pub async fn main() -> Result<()> {
+    // Open a connection to the mini-redis address.
+    let mut client = client::connect("127.0.0.1:6379").await?;
+
+    // Set the key "hello" with value "world"
+    client.set("hello", "world".into()).await?;
+
+    // Get key "hello"
+    let result = client.get("hello").await?;
+
+    println!("got value from the server; result={:?}", result);
+
+    Ok(())
+}
